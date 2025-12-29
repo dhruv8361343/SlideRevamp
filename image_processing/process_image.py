5c946f9f5958f59c8748c6b3d905e89bd71fdff4
Dhruv Gupta <dhruv_g@mfs.iitr.ac.in>
2025-12-29 21:16:40 +0530

    Refactor process_image function for clarity

--- image_processing/process_image.py
+++ image_processing/process_image.py
@@ -6,49 +6,42 @@ from image_processing.mask import generate_mask
 from image_processing.smart_crop import smart_crop
 
 
-
-
 def process_image(image_path):
     """
-    using models for enhancing image
+    Using models for enhancing image
     """
-    # Directories
-UPSCALED_DIR = Path("/kaggle/working/outputs/images_upscaled")
-MASKS_DIR = Path("/kaggle/working/outputs/masks")
-FINAL_DIR = Path("/kaggle/working/outputs/images_final")
 
-# Ensure output dirs exist
-UPSCALED_DIR.mkdir(parents=True, exist_ok=True)
-MASKS_DIR.mkdir(parents=True, exist_ok=True)
-FINAL_DIR.mkdir(parents=True, exist_ok=True)
     
-
-   # Upscale (Real-ESRGAN)
-upscaled_path = upscale_image(
-    image_path=image_path,
-    output_dir=UPSCALED_DIR
-)
-    
+    UPSCALED_DIR = Path("/kaggle/working/outputs/images_upscaled")
+    MASKS_DIR = Path("/kaggle/working/outputs/masks")
+    FINAL_DIR = Path("/kaggle/working/outputs/images_final")
 
 
-   #  Generate U-2-Net mask
-mask_path = generate_mask(
-    image_path=upscaled_path,
-    output_dir=MASKS_DIR
-)
+    UPSCALED_DIR.mkdir(parents=True, exist_ok=True)
+    MASKS_DIR.mkdir(parents=True, exist_ok=True)
+    FINAL_DIR.mkdir(parents=True, exist_ok=True)
 
-   #  Smart crop
-final_img = smart_crop(
-    image_path=str(upscaled_path),
-    mask_path=str(mask_path),
-    target_ratio=16/9
-)
+    # Upscale (Real-ESRGAN)
+    upscaled_path = upscale_image(
+        image_path=image_path,
+        output_dir=UPSCALED_DIR
+    )
 
+    # Generate U-2-Net mask 
+    mask_path = generate_mask(
+        image_path=upscaled_path,
+        output_dir=MASKS_DIR
+    )
 
-    #  Save final image
-final_out = FINAL_DIR / img_path.name
-cv2.imwrite(str(final_out), final_img)
-return final_out
+    # Smart crop 
+    final_img = smart_crop(
+        image_path=str(upscaled_path),
+        mask_path=str(mask_path),
+        target_ratio=16/9
+    )
 
+    # Save final image
+    final_out = FINAL_DIR / Path(image_path).name
+    cv2.imwrite(str(final_out), final_img)
 
-
+    return final_out
